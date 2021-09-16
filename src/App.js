6aab3961ba46b7bfdcd29e5048d75b66edf47700
d4nielj <d4niel.djm@gmail.com>
2021-09-16 13:04:25 -0500

    Reboot repo

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import styled from 'styled-components';
-import { BouncingBall, Canvas, TransitionFromDot, CirclingBall } from './Features/index';
-
-const SApp = styled.div`
-  background: #191919;
-  min-width: 100vw;
-  min-height: 100vh;
-  padding-top: 5rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: #fff;
-`;
-
-const Paragraph = styled.p`
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.5;
-`;
-
 const App = () => (
-  <SApp>
-    <Paragraph>Tap the circle</Paragraph>
-    <Canvas>
-      <TransitionFromDot />
-    </Canvas>
-    <Paragraph>Bouncing ball</Paragraph>
-    <Canvas>
-      <BouncingBall />
-    </Canvas>
-    <Paragraph>Orbiting ball</Paragraph>
-    <Canvas>
-      <CirclingBall />
-    </Canvas>
-  </SApp>
+  <div>
+    <h1>Hello world</h1>
+  </div>
 );
 
 export default App;
--- src/Features/index.js
+++
@@ -1,6 +0,0 @@
-import Canvas from './canvas/canvas';
-import BouncingBall from './bouncingBall/bouncingBall';
-import CirclingBall from './circlingBall/circlingBall';
-import TransitionFromDot from './transition/transitionFromDot';
-
-export { Canvas, BouncingBall, CirclingBall, TransitionFromDot };
